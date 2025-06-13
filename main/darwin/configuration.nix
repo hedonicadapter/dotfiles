f6e81e96c482b4157d279f3a2be7f424207d30fa
hedonicadapter <sam.herman@xenit.se>
2025-06-13 12:21:49 +0200

    flake updates, key repeat values, and spotify removed as standalone package

--- main/darwin/configuration.nix
+++ main/darwin/configuration.nix
@@ -100,8 +100,8 @@
       };
 
       NSGlobalDomain = {
-        KeyRepeat = 1;
-        InitialKeyRepeat = 1;
+        KeyRepeat = 2; # steps in GUI: 120, 90, 60, 30, 12, 6, 2
+        InitialKeyRepeat = 7; # steps in GUI: 120, 94, 68, 35, 25, 15
         AppleShowAllFiles = true; # finder hidden files
         NSWindowShouldDragOnGesture = true; # ctrl + cmd + drag = drag windows
       };
@@ -123,7 +123,7 @@
                 NightStartMinute = 0;
               };
               BlueReductionAvailable = 1;
-              BlueReductionEnabled = 0;
+              BlueReductionEnabled = 1;
               BlueReductionMode = 1;
               BlueReductionSunScheduleAllowed = 1;
               Version = 1;
